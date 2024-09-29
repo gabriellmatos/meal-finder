30a5d571a4d86e19554332a7fef2a929ca2e4e00
gabriellmatos <gabriel.lz.mts@gmail.com>
2024-09-29 15:28:12 -0300

    show single meal page

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ function searchMeal(e) {
 		fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
 			.then((res) => res.json())
 			.then((data) => {
-				console.log(data);
 				resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
 
 				if (data.meals === null) {
@@ -46,4 +45,67 @@ function searchMeal(e) {
 	}
 }
 
+// Fetch meal by ID
+function getMealById(mealID) {
+	fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+		.then((res) => res.json())
+		.then((data) => {
+			if (data.meals) {
+				const meal = data.meals[0];
+
+				addMealToDOM(meal);
+				// single_mealEl.innerHTML = data.meals[0].strInstructions;
+			}
+		});
+}
+
+// Add meal to DOM
+function addMealToDOM(meal) {
+	const ingredients = [];
+
+	for (let i = 1; i <= 20; i++) {
+		if (meal[`strIngredient${i}`]) {
+			ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+		} else {
+			break;
+		}
+	}
+	console.log(meal);
+	single_mealEl.innerHTML = `
+		<div class="single-meal">
+			<h1>${meal.strMeal}</h1>
+			<img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+			<div class="single-meal-info">
+				${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+				${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+			</div>
+			<div class="main">
+				<p>Instruction</p>
+				${meal.strInstructions}
+				<h2>Ingredients</h2>
+				<ul>
+					${ingredients.map((ingredient) => `<li>${ingredient}</li>`).join('')}
+				</ul>
+			</div>
+		</div>
+	`;
+}
+
+// Event listeners
+
 submit.addEventListener('submit', searchMeal);
+
+mealsEl.addEventListener('click', (e) => {
+	const mealInfo = e.composedPath().find((item) => {
+		if (item.classList) {
+			return item.classList.contains('meal-info');
+		} else {
+			return false;
+		}
+	});
+
+	if (mealInfo) {
+		const mealID = mealInfo.getAttribute('data-mealID');
+		getMealById(mealID);
+	}
+});
